78d446f0a9713887d96aa0d4bce5b9e63b972d74
David Morgan <david.alexandermorgan@gmail.com>
2018-08-03 00:18:04 +0000

    loadedDie assignment complete

---
+++ loadedDie.js
@@ -0,0 +1,22 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var index = 0;
+
+  return function() {
+
+    if(index < list.length){
+      index++;
+    } else {
+      index = 1;
+    }
+
+    return list[index-1];
+
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
